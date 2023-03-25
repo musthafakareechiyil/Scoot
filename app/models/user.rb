847381c9f0b3d3de50826dd5e81a63551aaf5338
musthafakareechiyil <musthafakareechiyil@gmail.com>
2023-03-25 17:38:26 +0530

    fails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  # Validate username presence and uniqueness
+  validates :username, presence: { message: "Username can't be blank" }, 
+                        uniqueness: { case_sensitive: false, message: "Username must be unique" }, 
+                        length: { minimum: 3, message: "Username must be at least 3 characters long" }
+
+  # Validate email presence and format
+  validates :email, presence: { message: "Email can't be blank" }, 
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email format is invalid" }
+
+  # Validate phone presence and format
+  validates :phone, presence: { message: "Phone can't be blank" },
+                     format: { with: /\A\d{10}\z/, message: "Phone format is invalid. It should be 10 digits" }
+
+  # Validate password presence, length and confirmation
+  validates :password, presence: { message: "Password can't be blank" }, length: { minimum: 6, message: "Password must be at least 6 characters long" }
+  validates_confirmation_of :password, message: "Password and password confirmation do not match"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
